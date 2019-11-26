526a0ec7feed3f3a8efaa01ccbe6e1280b86deac
Andreas Brandhøj <andreasbrandhoej@hotmail.com>
2019-11-26 08:34:29 +0100

    Edge Update
    Edge now contains information about te verticies and the struct and its members are now commented.

--- Src/Edge.h
+++ Src/Edge.h
@@ -1,6 +1,22 @@
 #pragma twice
 
-/* hub hvilke er forbundet? */
-typedef struct edge {
-  double distance;
+/**
+ * \struct This struct caontains the information used to connect verticies in the graph.
+ */
+typedef struct {
+
+    /**
+     * This is one of the verticies that is conencted by this edge
+    */
+    int u;
+
+    /**
+     * This is one of the verticies that is conencted by this edge
+    */
+    int v;
+
+    /**
+     * This is the distance between the two verticies
+    */
+    double distance;
 } edge;
