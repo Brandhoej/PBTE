fcd1ae5535dc811721430fcc364674051e75e9a6
Andreas Brandhøj <andreasbrandhoej@hotmail.com>
2019-11-26 08:47:13 +0100

    Graph comments
    Added doxygen formatted comments to graph.

--- Src/Graph.h
+++ Src/Graph.h
@@ -2,12 +2,36 @@
 
 #include "Hub.h"
 #include "Edge.h"
-
-typedef struct graph {
+'
+/**
+ * \struct This struct contains the nessecary members to have a functional undirected graph
+*/
+typedef struct {
+    /**
+     * The amount of hubs in this graph 
+     * @TODO make more generic
+    */
 	unsigned int hubAmount;
+
+    /**
+     * The hub array. Which is a colelction of the verticies in this graph
+    */
 	hub *hubs;
+
+    /**
+     * The edges in this graph. This is an adjacency matrix. 
+     * Where edges[0] returns an array of size hubAmount with all the edges. 
+     * Fx. edges[0][1] is the edge between vertex 0 and vertex 1.
+     * 
+    */
 	edge **edges;
 } graph;
 
+/**
+ * This function initializes a graph with hubs and creates the adjacency matrix
+ * @param g is the graph pointer that will be initialized by this function
+ * @param hubs is an array of the ubs this graph should contain
+ * @param hubAmount an unsigned amount of hubs in the hubs
+*/
 void initGraph(graph *g, hub *hubs, unsigned int hubAmount);
 void addEdge(graph *g, int from, int to, edge *edge);
