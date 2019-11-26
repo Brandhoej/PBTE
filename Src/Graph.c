d553f487330ac508c71615f5833e0d2fbd642539
Andreas Brandhøj <andreasbrandhoej@hotmail.com>
2019-11-26 18:43:08 +0100

    Ændringer på graf med uvis retning
    Vi ved ikke helt hvad vi har gang i og derfor er denne lidt rodet.

--- Src/Graph.c
+++ Src/Graph.c
@@ -1,18 +1,41 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "Graph.h"
 
 void initGraph(graph *graph, hub *hubs, unsigned int hubAmount) {
+  int e = 0;
+  graph->hubAmount = hubAmount;
+  graph->hubs = hubs;
+  printf("%i\n", sizeof(edge));
+  /* (HA - 1) * (min - 1) + (max - 1)*/
+  
+  
+  /*
     int e;
+    printf("TT1\n");
     graph->hubAmount = hubAmount;
+    printf("TT2\n");
     graph->hubs = hubs;
+    printf("TT3\n");
     graph->edges = calloc(hubAmount, sizeof(edge*));
+    printf("TT4\n");
+    printf("%i\n", sizeof(edge));
+    printf("TT4\n");
     for (e = 0; e < hubAmount; ++e) {
         graph->edges[e] = calloc(hubAmount, sizeof(edge));
     }
+  */
 }
 
-void addEdge(graph *graph, edge *edge) {
-    graph->edges[edge->u][edge->v] = *edge;
-    graph->edges[edge->v][edge->u] = *edge;
+void addEdge(graph *graph, int u, int v, edge *edge) {
+  /*
+    graph->edges[u][v] = *edge;
+    graph->edges[v][u] = *edge;
+  */
 }
 
+void getEdge(graph *graph, int u, int v, edge *edge){
+  /*
+  edge = &(graph->edges[u][v]);
+  */
+}
--- Src/Graph.h
+++ Src/Graph.h
@@ -22,9 +22,9 @@ typedef struct {
      * The edges in this graph. This is an adjacency matrix.
      * Where edges[0] returns an array of size hubAmount with all the edges.
      * Fx. edges[0][1] is the edge between vertex 0 and vertex 1.
-     *
+     * @TODO this supports directed graphs but we are using undirected.
     */
-	edge **edges;
+	edge *edges;
 } graph;
 
 /**
@@ -38,6 +38,16 @@ void initGraph(graph *graph, hub *hubs, unsigned int hubAmount);
 /**
  * Function prototype, adds edge to the graph using parameters
  * @param graph is pointer to a graph, where edge are added
+ * @param u is one of the verticies
+ * @param v is one of the verticies
  * @param edge struct containing the connections between hubs
  */
-void addEdge(graph *graph, edge *edge);
+void addEdge(graph *graph, int u, int v, edge *edge);
+
+/**
+ * Returns the edge connecting these verticies in an undirected graph.
+ * @param u is one of the verticies
+ * @param v is one of the verticies
+ * @param edge output
+ */
+void getEdge(graph *graph, int u, int v, edge *edge);
