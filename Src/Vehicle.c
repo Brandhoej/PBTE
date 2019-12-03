fded56b23922c2fdb8af2a526c4c34119616b376
Andreas Brandhøj <andreasbrandhoej@hotmail.com>
2019-12-03 15:45:01 +0100

    Added comments

--- Src/Vehicle.c
+++ Src/Vehicle.c
@@ -1,13 +1,10 @@
-#include "Vehicle.h"
 #include <stdio.h>
+#include "Vehicle.h"
 
 unsigned int availableCapacity(Vehicle *vehicle) {
     return vehicle->capacity - vehicle->inventory;
 }
 
-/**
- * @returns a negative number if the vehicle must deliver at the hub. If positive the vehicle takes inventory from the hub.
- */
 int getVehicleActionAtHub(Hub *hub, Vehicle *vehicle){
     int balance = getBalance(hub), action = 0;
     if(balance < 0){
